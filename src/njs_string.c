af55d443a3bd3fcc171ff3570b4c239e7a00ab80
Alexander Borisov <alexander.borisov@nginx.com>
2020-08-26 21:11:03 +0300

    Fixed return value for string base64/base64url encoding.

--- src/njs_string.c
+++ src/njs_string.c
@@ -317,7 +317,7 @@ njs_string_base64(njs_vm_t *vm, njs_value_t *value, const njs_str_t *src)
 
     dst.length = njs_base64_encoded_length(src->length);
 
-    dst.start = njs_string_alloc(vm, &vm->retval, dst.length, dst.length);
+    dst.start = njs_string_alloc(vm, value, dst.length, dst.length);
     if (njs_slow_path(dst.start == NULL)) {
         return NJS_ERROR;
     }
@@ -348,7 +348,7 @@ njs_string_base64url(njs_vm_t *vm, njs_value_t *value, const njs_str_t *src)
 
     dst.length = njs_base64_encoded_length(src->length) - padding;
 
-    dst.start = njs_string_alloc(vm, &vm->retval, dst.length, dst.length);
+    dst.start = njs_string_alloc(vm, value, dst.length, dst.length);
     if (njs_slow_path(dst.start == NULL)) {
         return NJS_ERROR;
     }
