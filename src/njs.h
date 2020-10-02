28cd05f79a4c1e1978377eef1ed2cb4ad0d1712c
Dmitry Volyntsev <xeioex@nginx.com>
2020-10-02 12:18:41 +0000

    Version bump.

--- src/njs.h
+++ src/njs.h
@@ -11,7 +11,7 @@
 
 #include <njs_auto_config.h>
 
-#define NJS_VERSION                 "0.4.4"
+#define NJS_VERSION                 "0.5.0"
 
 
 #include <unistd.h>                 /* STDOUT_FILENO, STDERR_FILENO */
