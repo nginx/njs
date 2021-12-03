09f176c255f9bf665a66959efcb06d3464879b0b
Dmitry Volyntsev <xeioex@nginx.com>
2021-12-03 13:55:11 +0000

    Added memory exception if alloc fails for njs_vm_function_alloc.

--- src/njs_function.c
+++ src/njs_function.c
@@ -72,6 +72,7 @@ njs_vm_function_alloc(njs_vm_t *vm, njs_function_native_t native)
 
     function = njs_mp_zalloc(vm->mem_pool, sizeof(njs_function_t));
     if (njs_slow_path(function == NULL)) {
+        njs_memory_error(vm);
         return NULL;
     }
 
