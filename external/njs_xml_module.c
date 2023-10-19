e21974eefc00e4e5959aaf49de42e982501c1e9e
Dmitry Volyntsev <xeioex@nginx.com>
2023-10-18 18:36:00 -0700

    XML: fixed compilation with certain GCC versions.
    external/njs_xml_module.c:541:16: error: 'name.length' may be used
    uninitialized [-Werror=maybe-uninitialized]
      541 |             if (name.length != njs_strlen(node->name).

--- external/njs_xml_module.c
+++ external/njs_xml_module.c
@@ -527,6 +527,11 @@ njs_xml_doc_ext_root(njs_vm_t *vm, njs_object_prop_t *prop, njs_value_t *value,
             njs_value_undefined_set(retval);
             return NJS_DECLINED;
         }
+
+    } else {
+        /* To suppress warning. */
+        name.length = 0;
+        name.start = NULL;
     }
 
     for (node = xmlDocGetRootElement(tree->doc);
