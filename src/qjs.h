nginx/njs

commit d1c615eaa208a4e7c92b661f8f7674e92f19aedf

QuickJS: added buffer module.

src/qjs.h
@@ -16,6 +16,7 @@
 #include <njs_unicode.h>
 #include <njs_utf8.h>
 #include <njs_chb.h>
+#include <njs_utils.h>
 
 #if defined(__GNUC__) && (__GNUC__ >= 8)
 #pragma GCC diagnostic push
@@ -43,6 +44,7 @@ JSContext *qjs_new_context(JSRuntime *rt, _Bool eval);
 
 
 JSValue qjs_buffer_alloc(JSContext *ctx, size_t size);
+JSValue qjs_buffer_create(JSContext *ctx, u_char *start, size_t size);
 JSValue qjs_buffer_chb_alloc(JSContext *ctx, njs_chb_t *chain);
 
 typedef int (*qjs_buffer_encode_t)(JSContext *ctx, const njs_str_t *src,

test/harness/runTsuite.js
@@ -48,6 +48,9 @@ function merge(to, from) {
             } else if (from[v] instanceof Uint8Array) {
                 r[v] = new Uint8Array(from[v]);
 
+            } else if (from[v] instanceof ArrayBuffer) {
+                r[v] = new ArrayBuffer(from[v].byteLength);
+
             } else {
                 r[v] = Object.assign(Array.isArray(from[v]) ? [] : {}, from[v]);
             }

commit 979c2af0c92d402efc83b95fefe2d6e6cd691655

Fixed compilation on 32bit platforms.
The issue was introduced in d1c615eaa.

src/qjs.h
@@ -11,6 +11,7 @@
 
 #include <njs_types.h>
 #include <njs_clang.h>
+#include <inttypes.h>
 #include <string.h>
 #include <njs_str.h>
 #include <njs_unicode.h>

src/qjs_buffer.c
@@ -760,7 +760,7 @@ qjs_buffer_array_range(JSContext *ctx, njs_str_t *array, JSValueConst start,
     }
 
     if (num_start < 0 || (size_t) num_start > array->length) {
-        return JS_ThrowRangeError(ctx, "\"%sStart\" is out of range: %ld",
+        return JS_ThrowRangeError(ctx, "\"%sStart\" is out of range: %" PRId64,
                                   name, num_start);
     }
 
@@ -773,7 +773,7 @@ qjs_buffer_array_range(JSContext *ctx, njs_str_t *array, JSValueConst start,
     }
 
     if (num_end < 0 || (size_t) num_end > array->length) {
-        return JS_ThrowRangeError(ctx, "\"%sEnd\" is out of range: %ld",
+        return JS_ThrowRangeError(ctx, "\"%sEnd\" is out of range: %" PRId64,
                                   name, num_end);
     }
 
@@ -1141,8 +1141,8 @@ qjs_buffer_prototype_read_float(JSContext *ctx, JSValueConst this_val,
     size = magic >> 2;
 
     if (size + index > self.length) {
-        return JS_ThrowRangeError(ctx, "index %lu is outside the bound of the"
-                                  " buffer", index);
+        return JS_ThrowRangeError(ctx, "index %" PRIu64 " is outside the bound"
+                                  " of the buffer", index);
     }
 
     little = magic & 1;
@@ -1217,8 +1217,8 @@ qjs_buffer_prototype_read_int(JSContext *ctx, JSValueConst this_val,
     }
 
     if (size + index > self.length) {
-        return JS_ThrowRangeError(ctx, "index %lu is outside the bound of the"
-                                  " buffer", index);
+        return JS_ThrowRangeError(ctx, "index %" PRIu64 " is outside the bound"
+                                  " of the buffer", index);
     }
 
     sign = (magic >> 1) & 1;
@@ -1628,8 +1628,8 @@ qjs_buffer_prototype_write_int(JSContext *ctx, JSValueConst this_val,
     }
 
     if (size + index > self.length) {
-        return JS_ThrowRangeError(ctx, "index %lu is outside the bound of the"
-                                  " buffer", index);
+        return JS_ThrowRangeError(ctx, "index %" PRIu64 " is outside the bound"
+                                  " of the buffer", index);
     }
 
     little = magic & 1;
@@ -1834,8 +1834,8 @@ qjs_buffer_prototype_write_float(JSContext *ctx, JSValueConst this_val,
     size = magic >> 2;
 
     if (size + index > self.length) {
-        return JS_ThrowRangeError(ctx, "index %lu is outside the bound of the"
-                                  " buffer", index);
+        return JS_ThrowRangeError(ctx, "index %" PRIu64 " is outside the bound"
+                                  " of the buffer", index);
     }
 
     little = magic & 1;