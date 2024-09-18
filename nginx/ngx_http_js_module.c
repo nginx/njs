nginx/njs

commit a14be61c86db926c93645e929a79a960674b7715

HTTP: simplified check for subrequest from a subrequest.

nginx/ngx_http_js_module.c
@@ -3153,7 +3153,7 @@ ngx_http_js_ext_subrequest(njs_vm_t *vm, njs_value_t *args, njs_uint_t nargs,
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_js_module);
 
-    if (ctx->vm != vm) {
+    if (r->main != r) {
         njs_vm_error(vm, "subrequest can only be created for "
                          "the primary request");
         return NJS_ERROR;
@@ -3511,7 +3511,7 @@ ngx_http_js_ext_get_parent(njs_vm_t *vm, njs_object_prop_t *prop,
     ctx = r->parent ? ngx_http_get_module_ctx(r->parent, ngx_http_js_module)
                     : NULL;
 
-    if (ctx == NULL || ctx->vm != vm) {
+    if (ctx == NULL) {
         njs_value_undefined_set(retval);
         return NJS_DECLINED;
     }

nginx/t/js_subrequests.t
@@ -590,6 +590,9 @@ local $TODO = 'not yet' unless has_version('0.8.5');
 
 http_get('/sr_error_in_callback');
 
+ok(index($t->read_file('error.log'), 'subrequest can only be created for') > 0,
+   'subrequest creation failed');
+
 }
 
 $t->stop();
@@ -602,8 +605,6 @@ ok(index($t->read_file('error.log'), 'failed to convert options.args') > 0,
 	'subrequest invalid args exception');
 ok(index($t->read_file('error.log'), 'too big subrequest response') > 0,
 	'subrequest too large body');
-ok(index($t->read_file('error.log'), 'subrequest creation failed') > 0,
-	'subrequest creation failed');
 ok(index($t->read_file('error.log'),
 		'js subrequest: failed to get the parent context') > 0,
 	'zero parent ctx');

commit b2de947243834de2dfaeaec8597b79fe649ebd4a

HTTP: fixed r.subrequest() check for nested subrequests.
The issue was introduced in a14be61c86 (0.8.5).

This fixes #783 on Github.

nginx/ngx_http_js_module.c
@@ -3358,7 +3358,7 @@ ngx_http_js_ext_subrequest(njs_vm_t *vm, njs_value_t *args, njs_uint_t nargs,
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_js_module);
 
-    if (r->main != r) {
+    if (r->subrequest_in_memory) {
         njs_vm_error(vm, "subrequest can only be created for "
                          "the primary request");
         return NJS_ERROR;
@@ -5722,7 +5722,7 @@ ngx_http_qjs_ext_subrequest(JSContext *cx, JSValueConst this_val,
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_js_module);
 
-    if (r->main != r) {
+    if (r->subrequest_in_memory) {
         return JS_ThrowTypeError(cx, "subrequest can only be created for "
                                      "the primary request");
     }