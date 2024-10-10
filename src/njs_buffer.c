nginx/njs

commit 5d15a8d6dabb44afefd5308e0ebde0cf73597b88

Fixed Buffer.prototype.lastIndexOf().

src/njs_buffer.c
@@ -1981,7 +1981,7 @@ njs_buffer_prototype_index_of(njs_vm_t *vm, njs_value_t *args, njs_uint_t nargs,
     njs_index_t last, njs_value_t *retval)
 {
     uint8_t                      byte;
-    int64_t                      from, to, increment, length, offset, index, i;
+    int64_t                      from, to, increment, length, index, i;
     njs_int_t                    ret;
     njs_str_t                    str;
     njs_value_t                  *this, *value, *value_from, *enc, dst;
@@ -2070,8 +2070,7 @@ encoding:
         return NJS_ERROR;
     }
 
-    u8 = &buffer->u.u8[0];
-    offset = array->offset;
+    u8 = &buffer->u.u8[array->offset];
 
     switch (value->type) {
     case NJS_STRING:
@@ -2104,23 +2103,21 @@ encoding:
             goto done;
         }
 
-        if (last) {
-            if (from - to < (int64_t) str.length) {
-                goto done;
-            }
+        if (str.length > (size_t) length) {
+            goto done;
+        }
 
+        if (last) {
             from -= str.length - 1;
+            from = njs_max(from, 0);
 
         } else {
-            if (to - from < (int64_t) str.length) {
-                goto done;
-            }
-
             to -= str.length - 1;
+            to = njs_min(to, length);
         }
 
         for (i = from; i != to; i += increment) {
-            if (memcmp(&u8[offset + i], str.start, str.length) == 0) {
+            if (memcmp(&u8[i], str.start, str.length) == 0) {
                 index = i;
                 goto done;
             }
@@ -2132,7 +2129,7 @@ encoding:
         byte = njs_number_to_uint32(njs_number(value));
 
         for (i = from; i != to; i += increment) {
-            if (u8[offset + i] == byte) {
+            if (u8[i] == byte) {
                 index = i;
                 goto done;
             }

src/test/njs_unit_test.c
@@ -21550,6 +21550,9 @@ static njs_unit_test_t  njs_buffer_module_test[] =
               "buf.lastIndexOf('C')"),
       njs_str("2") },
 
+    { njs_str("Buffer.from('abcdef').lastIndexOf('abc', 1)"),
+      njs_str("0") },
+
     { njs_str("['swap16', 'swap32', 'swap64'].every(method => {"
               "    var buf = Buffer.from([]);"
               "    buf[method]();"

commit 3a808d73b63b58cd11caeac70bd384b237b6d7d6

Fixed Buffer.prototype.lastIndexOf() when `from` is provided.
Previous incorrect fix was introduced in 5d15a8d6 (0.8.5).

src/njs_buffer.c
@@ -2218,13 +2218,12 @@ encoding:
             goto done;
         }
 
-        if (str.length > (size_t) length) {
-            goto done;
-        }
-
         if (last) {
-            from -= str.length - 1;
-            from = njs_max(from, 0);
+            from = njs_min(from, length - (int64_t) str.length);
+
+            if (to > from) {
+                goto done;
+            }
 
         } else {
             to -= str.length - 1;
@@ -2245,6 +2244,10 @@ encoding:
 
     case NJS_NUMBER:
         byte = njs_number_to_uint32(njs_number(value));
+
+        if (last) {
+            from = njs_min(from, length - 1);
+        }
 
         for (i = from; i != to; i += increment) {
             if (u8[i] == byte) {

src/qjs_buffer.c
@@ -1047,6 +1047,10 @@ qjs_buffer_prototype_index_of(JSContext *ctx, JSValueConst this_val, int argc,
             return JS_EXCEPTION;
         }
 
+        if (last) {
+            from = njs_min(from, length - 1);
+        }
+
         for (i = from; i != to; i += increment) {
             if (self.start[i] == (uint8_t) byte) {
                 return JS_NewInt32(ctx, i);
@@ -1087,14 +1091,12 @@ encoding:
         return JS_NewInt32(ctx, (last) ? length : 0);
     }
 
-    if (str.length > (size_t) length) {
-        JS_FreeValue(ctx, buffer);
-        return JS_NewInt32(ctx, -1);
-    }
-
     if (last) {
-        from -= str.length - 1;
-        from = njs_max(from, 0);
+        from = njs_min(from, length - (int64_t) str.length);
+
+        if (to > from) {
+            goto done;
+        }
 
     } else {
         to -= str.length - 1;

test/buffer.t.js
@@ -583,6 +583,7 @@ let lastIndexOf_tsuite = {
         { buf: Buffer.from('abcdef'), value: 'def', expected: 3 },
         { buf: Buffer.from('abcdef'), value: 'abc', offset: 1, expected: 0 },
         { buf: Buffer.from('abcdef'), value: 'def', offset: 1, expected: -1 },
+        { buf: Buffer.from('xxxABCx'), value: 'ABC', offset: 3, expected: 3 },
         { buf: Buffer.from(Buffer.alloc(7).fill('Zabcdef').buffer, 1), value: 'abcdef', expected: 0 },
         { buf: Buffer.from(Buffer.alloc(7).fill('Zabcdef').buffer, 1), value: 'abcdefg', expected: -1 },
         { buf: Buffer.from('abcdef'), value: '626364', encoding: 'hex', expected: 1 },
@@ -590,6 +591,7 @@ let lastIndexOf_tsuite = {
           exception: 'TypeError: "utf-128" encoding is not supported' },
         { buf: Buffer.from('abcabc'), value: 0x61, expected: 3 },
         { buf: Buffer.from('abcabc'), value: 0x61, offset: 1, expected: 0 },
+        { buf: Buffer.from('ab'), value: 7, offset: 2, expected: -1 },
         { buf: Buffer.from('abcdef'), value: Buffer.from('def'), expected: 3 },
         { buf: Buffer.from('abcdef'), value: Buffer.from(new Uint8Array([0x60, 0x62, 0x63]).buffer, 1), expected: 1 },
         { buf: Buffer.from('abcdef'), value: {},