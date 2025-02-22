nginx/njs

commit 75ca26f8347123c3163439a3652c1218bcb3bfa7

QuickJS: added WebCrypto module.

src/qjs.h
@@ -18,6 +18,7 @@
 #include <njs_utf8.h>
 #include <njs_chb.h>
 #include <njs_utils.h>
+#include <njs_assert.h>
 
 #if defined(__GNUC__) && (__GNUC__ >= 8)
 #pragma GCC diagnostic push
@@ -35,12 +36,16 @@
 #define QJS_CORE_CLASS_ID_OFFSET    64
 #define QJS_CORE_CLASS_ID_BUFFER    (QJS_CORE_CLASS_ID_OFFSET)
 #define QJS_CORE_CLASS_ID_UINT8_ARRAY_CTOR (QJS_CORE_CLASS_ID_OFFSET + 1)
+#define QJS_CORE_CLASS_ID_OFFSET    64
+#define QJS_CORE_CLASS_ID_BUFFER    (QJS_CORE_CLASS_ID_OFFSET)
+#define QJS_CORE_CLASS_ID_UINT8_ARRAY_CTOR (QJS_CORE_CLASS_ID_OFFSET + 1)
 #define QJS_CORE_CLASS_ID_TEXT_DECODER (QJS_CORE_CLASS_ID_OFFSET + 2)
 #define QJS_CORE_CLASS_ID_TEXT_ENCODER (QJS_CORE_CLASS_ID_OFFSET + 3)
 #define QJS_CORE_CLASS_ID_FS_STATS  (QJS_CORE_CLASS_ID_OFFSET + 4)
 #define QJS_CORE_CLASS_ID_FS_DIRENT (QJS_CORE_CLASS_ID_OFFSET + 5)
 #define QJS_CORE_CLASS_ID_FS_FILEHANDLE (QJS_CORE_CLASS_ID_OFFSET + 6)
-#define QJS_CORE_CLASS_ID_LAST      (QJS_CORE_CLASS_ID_OFFSET + 7)
+#define QJS_CORE_CLASS_ID_WEBCRYPTO_KEY (QJS_CORE_CLASS_ID_OFFSET + 7)
+#define QJS_CORE_CLASS_ID_LAST      (QJS_CORE_CLASS_ID_OFFSET + 8)
 
 
 typedef JSModuleDef *(*qjs_addon_init_pt)(JSContext *ctx, const char *name);
@@ -55,6 +60,7 @@ JSContext *qjs_new_context(JSRuntime *rt, qjs_module_t **addons);
 
 
 JSValue qjs_new_uint8_array(JSContext *ctx, int argc, JSValueConst *argv);
+JSValue qjs_new_array_buffer(JSContext *cx, uint8_t *src, size_t len);
 JSValue qjs_buffer_alloc(JSContext *ctx, size_t size);
 JSValue qjs_buffer_create(JSContext *ctx, u_char *start, size_t size);
 JSValue qjs_buffer_chb_alloc(JSContext *ctx, njs_chb_t *chain);
@@ -74,6 +80,22 @@ typedef struct {
 
 const qjs_buffer_encoding_t *qjs_buffer_encoding(JSContext *ctx,
     JSValueConst value, JS_BOOL thrw);
+
+int qjs_base64_encode(JSContext *ctx, const njs_str_t *src,
+    njs_str_t *dst);
+size_t qjs_base64_encode_length(JSContext *ctx, const njs_str_t *src);
+int qjs_base64_decode(JSContext *ctx, const njs_str_t *src,
+    njs_str_t *dst);
+size_t qjs_base64_decode_length(JSContext *ctx, const njs_str_t *src);
+int qjs_base64url_encode(JSContext *ctx, const njs_str_t *src,
+    njs_str_t *dst);
+int qjs_base64url_decode(JSContext *ctx, const njs_str_t *src,
+    njs_str_t *dst);
+size_t qjs_base64url_decode_length(JSContext *ctx, const njs_str_t *src);
+int qjs_hex_encode(JSContext *ctx, const njs_str_t *src, njs_str_t *dst);
+size_t qjs_hex_encode_length(JSContext *ctx, const njs_str_t *src);
+int qjs_hex_decode(JSContext *ctx, const njs_str_t *src, njs_str_t *dst);
+size_t qjs_hex_decode_length(JSContext *ctx, const njs_str_t *src);
 
 JSValue qjs_process_object(JSContext *ctx, int argc, const char **argv);
 

auto/qjs_modules
@@ -13,6 +13,14 @@ njs_module_srcs=external/qjs_fs_module.c
 
 . auto/qjs_module
 
+if [ $NJS_OPENSSL = YES -a $NJS_HAVE_OPENSSL = YES ]; then
+	njs_module_name=qjs_webcrypto_module
+	njs_module_incs=
+	njs_module_srcs=external/qjs_webcrypto_module.c
+
+	. auto/qjs_module
+fi
+
 if [ $NJS_ZLIB = YES -a $NJS_HAVE_ZLIB = YES ]; then
     njs_module_name=qjs_zlib_module
     njs_module_incs=

nginx/config
@@ -117,6 +117,10 @@ if [ $NJS_OPENSSL != NO ]; then
     have=NJS_HAVE_OPENSSL . auto/have
     NJS_SRCS="$NJS_SRCS $ngx_addon_dir/../external/njs_webcrypto_module.c"
 
+    if [ "$NJS_HAVE_QUICKJS" = "YES" ];  then
+        NJS_SRCS="$NJS_SRCS $ngx_addon_dir/../external/qjs_webcrypto_module.c"
+    fi
+
     echo " enabled webcrypto module"
 fi
 

nginx/ngx_http_js_module.c
@@ -1141,6 +1141,9 @@ qjs_module_t *njs_http_qjs_addon_modules[] = {
      * Shared addons should be in the same order and the same positions
      * in all nginx modules.
      */
+#ifdef NJS_HAVE_OPENSSL
+    &qjs_webcrypto_module,
+#endif
 #ifdef NJS_HAVE_ZLIB
     &qjs_zlib_module,
 #endif

nginx/ngx_js.h
@@ -377,6 +377,7 @@ ngx_int_t ngx_qjs_string(ngx_engine_t *e, JSValueConst val, ngx_str_t *str);
 #define ngx_qjs_external_ctx(cx, e)                                          \
     ((ngx_js_external_ctx_pt) ngx_qjs_meta(cx, 11))(e)
 
+extern qjs_module_t  qjs_webcrypto_module;
 extern qjs_module_t  qjs_zlib_module;
 extern qjs_module_t  ngx_qjs_ngx_module;
 extern qjs_module_t  ngx_qjs_ngx_shared_dict_module;

nginx/ngx_stream_js_module.c
@@ -840,6 +840,9 @@ qjs_module_t *njs_stream_qjs_addon_modules[] = {
      * Shared addons should be in the same order and the same positions
      * in all nginx modules.
      */
+#ifdef NJS_HAVE_OPENSSL
+    &qjs_webcrypto_module,
+#endif
 #ifdef NJS_HAVE_ZLIB
     &qjs_zlib_module,
 #endif

nginx/t/js_webcrypto.t
@@ -1,0 +1,85 @@
+#!/usr/bin/perl
+
+# (C) Dmitry Volyntsev
+# (C) Nginx, Inc.
+
+# Tests for http njs module, WebCrypto module.
+
+###############################################################################
+
+use warnings;
+use strict;
+
+use Test::More;
+
+BEGIN { use FindBin; chdir($FindBin::Bin); }
+
+use lib 'lib';
+use Test::Nginx;
+
+###############################################################################
+
+select STDERR; $| = 1;
+select STDOUT; $| = 1;
+
+my $t = Test::Nginx->new()->has(qw/http/)
+	->write_file_expand('nginx.conf', <<'EOF');
+
+%%TEST_GLOBALS%%
+
+daemon off;
+
+events {
+}
+
+http {
+    %%TEST_GLOBALS_HTTP%%
+
+    js_import test.js;
+
+    server {
+        listen       127.0.0.1:8080;
+        server_name  localhost;
+
+        location /random_values_test {
+            js_content test.random_values_test;
+        }
+    }
+}
+
+EOF
+
+$t->write_file('test.js', <<EOF);
+    function count1(v) {
+        return v.toString(2).match(/1/g).length;
+    }
+
+    /*
+     * Statistic test
+     * bits1 is a random variable with Binomial distribution
+     * Expected value is N / 2
+     * Standard deviation is sqrt(N / 4)
+     */
+
+    function random_values_test(r) {
+        let buf = new Uint32Array(32);
+        crypto.getRandomValues(buf);
+        let bits1 = buf.reduce((a, v)=> a + count1(v), 0);
+        let nbits = buf.length * 32;
+        let mean = nbits / 2;
+        let stdd = Math.sqrt(nbits / 4);
+
+        r.return(200, bits1 > (mean - 10 * stdd) && bits1 < (mean + 10 * stdd));
+    }
+
+    export default {random_values_test};
+
+EOF
+
+$t->try_run('no njs')->plan(1);
+
+###############################################################################
+
+like(http_get('/random_values_test'), qr/true/, 'random_values_test');
+
+###############################################################################

commit ea03e28bbf63c8736a232424e169af4adeba75a6

Fixed typo introduced in 75ca26f.

src/qjs.h
@@ -36,9 +36,6 @@
 #define QJS_CORE_CLASS_ID_OFFSET    64
 #define QJS_CORE_CLASS_ID_BUFFER    (QJS_CORE_CLASS_ID_OFFSET)
 #define QJS_CORE_CLASS_ID_UINT8_ARRAY_CTOR (QJS_CORE_CLASS_ID_OFFSET + 1)
-#define QJS_CORE_CLASS_ID_OFFSET    64
-#define QJS_CORE_CLASS_ID_BUFFER    (QJS_CORE_CLASS_ID_OFFSET)
-#define QJS_CORE_CLASS_ID_UINT8_ARRAY_CTOR (QJS_CORE_CLASS_ID_OFFSET + 1)
 #define QJS_CORE_CLASS_ID_TEXT_DECODER (QJS_CORE_CLASS_ID_OFFSET + 2)
 #define QJS_CORE_CLASS_ID_TEXT_ENCODER (QJS_CORE_CLASS_ID_OFFSET + 3)
 #define QJS_CORE_CLASS_ID_FS_STATS  (QJS_CORE_CLASS_ID_OFFSET + 4)