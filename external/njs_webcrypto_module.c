nginx/njs

commit 637fc26eac8622ccf8c73cfa4604e9afe54c3f34

WebCrypto: making SHA-256 the default hash algorithm.

external/njs_webcrypto_module.c
@@ -4642,8 +4642,8 @@ njs_algorithm_hash(njs_vm_t *vm, njs_value_t *options,
 
     if (njs_value_is_object(options)) {
         val = njs_vm_object_prop(vm, options, &string_hash, &value);
-        if (njs_slow_path(val == NULL)) {
-            njs_value_undefined_set(njs_value_arg(&value));
+        if (val == NULL) {
+            return NJS_HASH_SHA256;
         }
 
     } else {

external/qjs_webcrypto_module.c
@@ -4437,6 +4437,11 @@ qjs_algorithm_hash(JSContext *cx, JSValue options, qjs_webcrypto_hash_t *hash)
 
     if (JS_IsObject(options)) {
         v = JS_GetPropertyStr(cx, options, "hash");
+        if (JS_IsUndefined(v)) {
+            *hash = QJS_HASH_SHA256;
+            return JS_UNDEFINED;
+        }
+
         if (JS_IsException(v)) {
             return JS_EXCEPTION;
         }

commit 4ce3965265260062a05cd8cc0aa56d3183478bdf

WebCrypto: fixed issue introduced in 637fc26e.
Found by Clang static analyzer.

external/njs_webcrypto_module.c
@@ -4852,7 +4852,8 @@ njs_algorithm_hash(njs_vm_t *vm, njs_value_t *options,
     if (njs_value_is_object(options)) {
         val = njs_vm_object_prop(vm, options, &string_hash, &value);
         if (val == NULL) {
-            return NJS_HASH_SHA256;
+            *hash = NJS_HASH_SHA256;
+            return NJS_OK;
         }
 
     } else {