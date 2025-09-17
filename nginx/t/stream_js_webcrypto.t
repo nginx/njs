nginx/njs

commit 7b3c8a66879dc0d5250bfeb53b2fbc85d15da637

Fetch: added keepalive support for ngx.fetch() API.
This closes #957 feature request on Github.

nginx/ngx_http_js_module.c
@@ -565,6 +565,34 @@ static ngx_command_t  ngx_http_js_commands[] = {
       0,
       NULL },
 
+    { ngx_string("js_fetch_keepalive"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_num_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_js_loc_conf_t, fetch_keepalive),
+      NULL },
+
+    { ngx_string("js_fetch_keepalive_requests"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_num_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_js_loc_conf_t, fetch_keepalive_requests),
+      NULL },
+
+    { ngx_string("js_fetch_keepalive_time"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_msec_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_js_loc_conf_t, fetch_keepalive_time),
+      NULL },
+
+    { ngx_string("js_fetch_keepalive_timeout"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_msec_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_js_loc_conf_t, fetch_keepalive_timeout),
+      NULL },
+
       ngx_null_command
 };
 

nginx/ngx_js.c
@@ -10,6 +10,7 @@
 #include <ngx_core.h>
 #include <math.h>
 #include "ngx_js.h"
+#include "ngx_js_http.h"
 
 
 typedef struct {
@@ -3986,6 +3987,11 @@ ngx_js_create_conf(ngx_conf_t *cf, size_t size)
     conf->max_response_body_size = NGX_CONF_UNSET_SIZE;
     conf->timeout = NGX_CONF_UNSET_MSEC;
 
+    conf->fetch_keepalive = NGX_CONF_UNSET_UINT;
+    conf->fetch_keepalive_requests = NGX_CONF_UNSET_UINT;
+    conf->fetch_keepalive_time = NGX_CONF_UNSET_MSEC;
+    conf->fetch_keepalive_timeout = NGX_CONF_UNSET_MSEC;
+
     return conf;
 }
 
@@ -4096,6 +4102,17 @@ ngx_js_merge_conf(ngx_conf_t *cf, void *parent, void *child,
     ngx_conf_merge_size_value(conf->buffer_size, prev->buffer_size, 16384);
     ngx_conf_merge_size_value(conf->max_response_body_size,
                               prev->max_response_body_size, 1048576);
+
+    ngx_conf_merge_uint_value(conf->fetch_keepalive, prev->fetch_keepalive, 0);
+    ngx_conf_merge_uint_value(conf->fetch_keepalive_requests,
+                              prev->fetch_keepalive_requests, 1000);
+    ngx_conf_merge_msec_value(conf->fetch_keepalive_time,
+                              prev->fetch_keepalive_time, 3600000);
+    ngx_conf_merge_msec_value(conf->fetch_keepalive_timeout,
+                              prev->fetch_keepalive_timeout, 60000);
+
+    ngx_queue_init(&conf->fetch_keepalive_cache);
+    ngx_queue_init(&conf->fetch_keepalive_free);
 
     if (ngx_js_merge_vm(cf, (ngx_js_loc_conf_t *) conf,
                         (ngx_js_loc_conf_t *) prev,

nginx/ngx_js.h
@@ -13,6 +13,7 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_event.h>
+#include <ngx_event_connect.h>
 #include <njs.h>
 #include <njs_rbtree.h>
 #include <njs_arr.h>
@@ -133,7 +134,14 @@ typedef struct {
                                                                               \
     size_t                 buffer_size;                                       \
     size_t                 max_response_body_size;                            \
-    ngx_msec_t             timeout
+    ngx_msec_t             timeout;                                           \
+                                                                              \
+    ngx_uint_t             fetch_keepalive;                                   \
+    ngx_uint_t             fetch_keepalive_requests;                          \
+    ngx_msec_t             fetch_keepalive_time;                              \
+    ngx_msec_t             fetch_keepalive_timeout;                           \
+    ngx_queue_t            fetch_keepalive_cache;                             \
+    ngx_queue_t            fetch_keepalive_free
 
 
 #if defined(NGX_HTTP_SSL) || defined(NGX_STREAM_SSL)

commit dcfd4d9104adbe3dad6f53b5df875aeaea55e1ab

Modules: fixed building when http_ssl and stream_ssl unavailable.
This fixes issue introduced in 7b3c8a66.

.github/workflows/check-pr.yml
@@ -12,7 +12,10 @@ jobs:
 
       - name: Set the defaults and set up environment
         run: |
-          echo NGINX_CONFIGURE_CMD="auto/configure --prefix=/tmp --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-select_module --with-poll_module --with-http_auth_request_module --with-http_v2_module --with-http_slice_module --with-stream --with-stream_ssl_module --with-stream_ssl_preread_module --with-stream_realip_module --with-threads --with-cpp_test_module --with-compat --with-http_degradation_module --with-http_xslt_module --with-http_image_filter_module --with-http_perl_module --with-http_geoip_module --with-stream_geoip_module" >> $GITHUB_ENV
+          NGINX_BASE="auto/configure --prefix=/tmp --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-select_module --with-poll_module --with-http_auth_request_module --with-http_v2_module --with-http_slice_module --with-stream --with-stream_realip_module --with-threads --with-cpp_test_module --with-compat --with-http_degradation_module --with-http_xslt_module --with-http_image_filter_module --with-http_perl_module --with-http_geoip_module --with-stream_geoip_module"
+          SSL_MODULES="--with-http_ssl_module --with-mail_ssl_module --with-stream_ssl_module --with-stream_ssl_preread_module"
+          echo NGINX_CONFIGURE_CMD="$NGINX_BASE $SSL_MODULES" >> $GITHUB_ENV
+          echo NGINX_CONFIGURE_CMD_NO_SSL="$NGINX_BASE" >> $GITHUB_ENV
           export DEB_BUILD_MAINT_OPTIONS="hardening=+all"
           export DEB_CFLAGS_MAINT_APPEND="-fPIC"
           export DEB_LDFLAGS_MAINT_APPEND=""-Wl,--as-needed""
@@ -124,6 +127,21 @@ jobs:
         env:
           TEST_NGINX_BINARY: "${{ github.workspace }}/nginx-source/objs/nginx"
           TEST_NGINX_GLOBALS: "load_module ${{ github.workspace }}/nginx-source/objs/ngx_http_js_module.so; load_module ${{ github.workspace }}/nginx-source/objs/ngx_stream_js_module.so;"
+          TEST_NGINX_VERBOSE: 1
+
+      - name: Configure and build nginx and njs modules, no SSL, static modules
+        run: |
+          cd nginx-source
+          NJS_OPENSSL=NO $NGINX_CONFIGURE_CMD_NO_SSL --with-cc-opt="$CC_OPT" --with-ld-opt="$LD_OPT" --add-module=../nginx || cat objs/autoconf.err
+          $MAKE_UTILITY -j$(nproc) modules
+          $MAKE_UTILITY -j$(nproc)
+
+      - name: Test njs modules, no SSL, static modules
+        run: |
+          ulimit -c unlimited
+          prove -v -j$(nproc) -Inginx-tests/lib --state=save nginx/t . || prove -v -Inginx-tests/lib --state=failed
+        env:
+          TEST_NGINX_BINARY: "${{ github.workspace }}/nginx-source/objs/nginx"
           TEST_NGINX_VERBOSE: 1
 
       - name: Create LSAN suppression file

nginx/ngx_http_js_module.c
@@ -400,7 +400,7 @@ static ngx_conf_bitmask_t  ngx_http_js_engines[] = {
     { ngx_null_string, 0 }
 };
 
-#if (NGX_HTTP_SSL)
+#if (NGX_SSL)
 
 static ngx_conf_bitmask_t  ngx_http_js_ssl_protocols[] = {
     { ngx_string("TLSv1"), NGX_SSL_TLSv1 },
@@ -519,7 +519,7 @@ static ngx_command_t  ngx_http_js_commands[] = {
       offsetof(ngx_http_js_loc_conf_t, timeout),
       NULL },
 
-#if (NGX_HTTP_SSL)
+#if (NGX_SSL)
 
     { ngx_string("js_fetch_ciphers"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
@@ -8172,7 +8172,7 @@ ngx_http_js_create_loc_conf(ngx_conf_t *cf)
         return NULL;
     }
 
-#if (NGX_HTTP_SSL)
+#if (NGX_SSL)
     conf->ssl_verify = NGX_CONF_UNSET;
     conf->ssl_verify_depth = NGX_CONF_UNSET;
 #endif

nginx/ngx_js.c
@@ -3996,7 +3996,7 @@ ngx_js_create_conf(ngx_conf_t *cf, size_t size)
 }
 
 
-#if defined(NGX_HTTP_SSL) || defined(NGX_STREAM_SSL)
+#if (NGX_SSL)
 
 static ngx_int_t
 ngx_js_merge_ssl(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
@@ -4122,7 +4122,7 @@ ngx_js_merge_conf(ngx_conf_t *cf, void *parent, void *child,
         return NGX_CONF_ERROR;
     }
 
-#if defined(NGX_HTTP_SSL) || defined(NGX_STREAM_SSL)
+#if (NGX_SSL)
 
     if (ngx_js_merge_ssl(cf, conf, prev) != NGX_OK) {
         return NGX_CONF_ERROR;

nginx/ngx_js.h
@@ -144,7 +144,7 @@ typedef struct {
     ngx_queue_t            fetch_keepalive_free
 
 
-#if defined(NGX_HTTP_SSL) || defined(NGX_STREAM_SSL)
+#if (NGX_SSL)
 #define NGX_JS_COMMON_LOC_CONF                                                \
     _NGX_JS_COMMON_LOC_CONF;                                                  \
                                                                               \

nginx/ngx_js_http.c
@@ -1643,9 +1643,11 @@ ngx_js_http_get_keepalive_connection(ngx_js_http_t *http)
             continue;
         }
 
+#if (NGX_SSL)
         if ((http->ssl != NULL) != (cache->ssl != 0)) {
             continue;
         }
+#endif
 
         if (ngx_strncasecmp(host->data, cache->host, host->len) != 0) {
             continue;
@@ -1775,7 +1777,9 @@ ngx_js_http_free_keepalive_connection(ngx_js_http_t *http)
 
     cache->connection = c;
 
+#if (NGX_SSL)
     cache->ssl = (http->ssl != NULL);
+#endif
     ngx_memcpy(cache->host, http->host.data, http->host.len);
     cache->host_len = http->host.len;
     cache->port = http->port;

nginx/ngx_stream_js_module.c
@@ -232,7 +232,7 @@ static ngx_conf_bitmask_t  ngx_stream_js_engines[] = {
     { ngx_null_string, 0 }
 };
 
-#if (NGX_STREAM_SSL)
+#if (NGX_SSL)
 
 static ngx_conf_bitmask_t  ngx_stream_js_ssl_protocols[] = {
     { ngx_string("TLSv1"), NGX_SSL_TLSv1 },
@@ -379,7 +379,7 @@ static ngx_command_t  ngx_stream_js_commands[] = {
       offsetof(ngx_stream_js_srv_conf_t, fetch_keepalive_timeout),
       NULL },
 
-#if (NGX_STREAM_SSL)
+#if (NGX_SSL)
 
     { ngx_string("js_fetch_ciphers"),
       NGX_STREAM_MAIN_CONF|NGX_STREAM_SRV_CONF|NGX_CONF_TAKE1,
@@ -3620,7 +3620,7 @@ ngx_stream_js_create_srv_conf(ngx_conf_t *cf)
         return NULL;
     }
 
-#if (NGX_STREAM_SSL)
+#if (NGX_SSL)
     conf->ssl_verify = NGX_CONF_UNSET;
     conf->ssl_verify_depth = NGX_CONF_UNSET;
 #endif

nginx/t/js_webcrypto.t
@@ -41,6 +41,10 @@ http {
         listen       127.0.0.1:8080;
         server_name  localhost;
 
+        location /has_crypto {
+            js_content test.has_crypto;
+        }
+
         location /random_values_test {
             js_content test.random_values_test;
         }
@@ -50,6 +54,10 @@ http {
 EOF
 
 $t->write_file('test.js', <<EOF);
+    function has_crypto(r) {
+        r.return(200, (crypto !== undefined).toString());
+    }
+
     function count1(v) {
         return v.toString(2).match(/1/g).length;
     }
@@ -72,11 +80,16 @@ $t->write_file('test.js', <<EOF);
         r.return(200, bits1 > (mean - 10 * stdd) && bits1 < (mean + 10 * stdd));
     }
 
-    export default {random_values_test};
+    export default {has_crypto, random_values_test};
 
 EOF
 
-$t->try_run('no njs')->plan(1);
+$t->try_run('no njs');
+
+plan(skip_all => 'njs crypto module not available')
+	if http_get('/has_crypto') !~ /true/;
+
+$t->plan(1);
 
 ###############################################################################
 

nginx/t/stream_js_webcrypto.t
@@ -23,7 +23,7 @@ use Test::Nginx::Stream qw/ stream /;
 select STDERR; $| = 1;
 select STDOUT; $| = 1;
 
-my $t = Test::Nginx->new()->has(qw/stream stream_return/)
+my $t = Test::Nginx->new()->has(qw/http stream stream_return/)
 	->write_file_expand('nginx.conf', <<'EOF');
 
 %%TEST_GLOBALS%%
@@ -31,6 +31,21 @@ my $t = Test::Nginx->new()->has(qw/stream stream_return/)
 daemon off;
 
 events {
+}
+
+http {
+    %%TEST_GLOBALS_HTTP%%
+
+    js_import test.js;
+
+    server {
+        listen       127.0.0.1:8080;
+        server_name  localhost;
+
+        location /has_crypto {
+            js_content test.has_crypto;
+        }
+    }
 }
 
 stream {
@@ -49,6 +64,10 @@ stream {
 EOF
 
 $t->write_file('test.js', <<EOF);
+    function has_crypto(r) {
+        r.return(200, (crypto !== undefined).toString());
+    }
+
     function count1(v) {
         return v.toString(2).match(/1/g).length;
     }
@@ -71,10 +90,15 @@ $t->write_file('test.js', <<EOF);
         return bits1 > (mean - 10 * stdd) && bits1 < (mean + 10 * stdd);
     }
 
-    export default {random_values_test};
+    export default {has_crypto, random_values_test};
 EOF
 
-$t->try_run('no stream js_var')->plan(1);
+$t->try_run('no stream js_var');
+
+plan(skip_all => 'njs crypto module not available')
+	if http_get('/has_crypto') !~ /true/;
+
+$t->plan(1);
 
 ###############################################################################
 