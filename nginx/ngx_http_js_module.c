nginx/njs

commit 04f6dfb91991a855095c41df760149d9e3847bd9

QuickJS: added njs.on('exit') API support.
This closes #955 issue on Github.

nginx/ngx_http_js_module.c
@@ -1609,7 +1609,7 @@ static ngx_int_t
 ngx_http_js_init_vm(ngx_http_request_t *r, njs_int_t proto_id)
 {
     ngx_http_js_ctx_t       *ctx;
-    ngx_pool_cleanup_t      *cln;
+    ngx_http_cleanup_t      *cln;
     ngx_http_js_loc_conf_t  *jlcf;
 
     jlcf = ngx_http_get_module_loc_conf(r, ngx_http_js_module);
@@ -1644,7 +1644,7 @@ ngx_http_js_init_vm(ngx_http_request_t *r, njs_int_t proto_id)
                    "http js vm clone %s: %p from: %p", jlcf->engine->name,
                    ctx->engine, jlcf->engine);
 
-    cln = ngx_pool_cleanup_add(r->pool, 0);
+    cln = ngx_http_cleanup_add(r, 0);
     if (cln == NULL) {
         return NGX_ERROR;
     }

nginx/t/js_exit.t
@@ -1,0 +1,76 @@
+#!/usr/bin/perl
+
+# (C) Dmitry Volyntsev
+# (C) F5, Inc.
+
+# Tests for http njs module, njs.on('exit', ...).
+
+###############################################################################
+
+use warnings;
+use strict;
+
+use Test::More;
+use Socket qw/ CRLF /;
+
+BEGIN { use FindBin; chdir($FindBin::Bin); }
+
+use lib 'lib';
+use Test::Nginx;
+
+###############################################################################
+
+select STDERR; $| = 1;
+select STDOUT; $| = 1;
+
+my $t = Test::Nginx->new()->has(qw/http/)
+	->write_file_expand('nginx.conf', <<'EOF');
+
+%%TEST_GLOBALS%%
+
+daemon off;
+
+events {
+}
+
+http {
+    %%TEST_GLOBALS_HTTP%%
+
+    js_import test.js;
+
+    server {
+        listen       127.0.0.1:8080;
+        server_name  localhost;
+
+        location /test {
+            js_content test.test;
+        }
+    }
+}
+
+EOF
+
+$t->write_file('test.js', <<EOF);
+    function test(r) {
+	    njs.on('exit', function() {
+		    ngx.log(ngx.WARN, `exit hook: bs: \${r.variables.bytes_sent}`);
+	    });
+
+	    r.return(200, `bs: \${r.variables.bytes_sent}`);
+    }
+
+    export default { test };
+
+EOF
+
+$t->try_run('no njs')->plan(2);
+
+###############################################################################
+
+like(http_get('/test'), qr/bs: 0/, 'response');
+
+$t->stop();
+
+like($t->read_file('error.log'), qr/\[warn\].*exit hook: bs: \d+/, 'exit hook logged');
+
+###############################################################################

external/njs_shell.c
@@ -2703,6 +2703,8 @@ njs_engine_qjs_destroy(njs_engine_t *engine)
     njs_queue_link_t        *link;
     njs_rejected_promise_t  *rejected_promise;
 
+    qjs_call_exit_hook(engine->u.qjs.ctx);
+
     console = JS_GetRuntimeOpaque(engine->u.qjs.rt);
 
     if (console->rejected_promises != NULL) {

nginx/ngx_js.c
@@ -1130,6 +1130,7 @@ ngx_engine_qjs_destroy(ngx_engine_t *e, ngx_js_ctx_t *ctx,
     uint32_t              i, length;
     ngx_str_t             exception;
     JSRuntime            *rt;
+    JSValue               ret;
     JSContext            *cx;
     JSClassID             class_id;
     JSMemoryUsage         stats;
@@ -1142,6 +1143,13 @@ ngx_engine_qjs_destroy(ngx_engine_t *e, ngx_js_ctx_t *ctx,
     cx = e->u.qjs.ctx;
 
     if (ctx != NULL) {
+        ret = qjs_call_exit_hook(cx);
+        if (JS_IsException(ret)) {
+            ngx_qjs_exception(e, &exception);
+            ngx_log_error(NGX_LOG_ERR, ctx->log, 0,
+                          "js exit hook exception: %V", &exception);
+        }
+
         node = njs_rbtree_min(&ctx->waiting_events);
 
         while (njs_rbtree_is_there_successor(&ctx->waiting_events, node)) {

nginx/t/stream_js_exit.t
@@ -108,8 +108,6 @@ EOF
 
 $t->try_run('no stream njs available');
 
-plan(skip_all => 'not yet') if http_get('/engine') =~ /QuickJS$/m;
-
 $t->plan(2);
 
 $t->run_daemon(\&stream_daemon, port(8090));

src/qjs.c
@@ -19,6 +19,12 @@ typedef struct {
 } qjs_signal_entry_t;
 
 
+typedef struct {
+#define QJS_NJS_HOOK_EXIT      0
+    JSValue         hooks[1];
+} qjs_njs_t;
+
+
 typedef enum {
     QJS_ENCODING_UTF8,
 } qjs_encoding_t;
@@ -42,7 +48,13 @@ typedef struct {
 extern char  **environ;
 
 
-static JSValue qjs_njs_getter(JSContext *ctx, JSValueConst this_val);
+static int qjs_add_intrinsic_njs(JSContext *cx, JSValueConst global);
+static JSValue qjs_njs_on(JSContext *ctx, JSValueConst this_val, int argc,
+    JSValueConst *argv);
+static void qjs_njs_mark(JSRuntime *rt, JSValueConst val,
+    JS_MarkFunc *mark_func);
+static void qjs_njs_finalizer(JSRuntime *rt, JSValue val);
+
 static JSValue qjs_process_env(JSContext *ctx, JSValueConst this_val);
 static JSValue qjs_process_kill(JSContext *ctx, JSValueConst this_val,
     int argc, JSValueConst *argv);
@@ -99,10 +111,6 @@ static qjs_encoding_label_t  qjs_encoding_labels[] =
 };
 
 
-static const JSCFunctionListEntry qjs_global_proto[] = {
-    JS_CGETSET_DEF("njs", qjs_njs_getter, NULL),
-};
-
 static const JSCFunctionListEntry qjs_text_decoder_proto[] = {
     JS_PROP_STRING_DEF("[Symbol.toStringTag]", "TextDecoder",
                        JS_PROP_CONFIGURABLE),
@@ -126,6 +134,7 @@ static const JSCFunctionListEntry qjs_njs_proto[] = {
     JS_PROP_INT32_DEF("version_number", NJS_VERSION_NUMBER,
                       JS_PROP_C_W_E),
     JS_PROP_STRING_DEF("engine", "QuickJS", JS_PROP_C_W_E),
+    JS_CFUNC_DEF("on", 2, qjs_njs_on),
 };
 
 static const JSCFunctionListEntry qjs_process_proto[] = {
@@ -134,6 +143,13 @@ static const JSCFunctionListEntry qjs_process_proto[] = {
     JS_CFUNC_DEF("kill", 2, qjs_process_kill),
     JS_CGETSET_DEF("pid", qjs_process_pid, NULL),
     JS_CGETSET_DEF("ppid", qjs_process_ppid, NULL),
+};
+
+
+static JSClassDef qjs_njs_class = {
+    "njs",
+    .finalizer = qjs_njs_finalizer,
+    .gc_mark = qjs_njs_mark,
 };
 
 
@@ -186,6 +202,10 @@ qjs_new_context(JSRuntime *rt, qjs_module_t **addons)
 
     global_obj = JS_GetGlobalObject(ctx);
 
+    if (qjs_add_intrinsic_njs(ctx, global_obj) < 0) {
+        return NULL;
+    }
+
     if (qjs_add_intrinsic_text_decoder(ctx, global_obj) < 0) {
         return NULL;
     }
@@ -193,9 +213,6 @@ qjs_new_context(JSRuntime *rt, qjs_module_t **addons)
     if (qjs_add_intrinsic_text_encoder(ctx, global_obj) < 0) {
         return NULL;
     }
-
-    JS_SetPropertyFunctionList(ctx, global_obj, qjs_global_proto,
-                               njs_nitems(qjs_global_proto));
 
     prop = JS_NewAtom(ctx, "eval");
     if (prop == JS_ATOM_NULL) {
@@ -225,20 +242,167 @@ qjs_new_context(JSRuntime *rt, qjs_module_t **addons)
 }
 
 
-static JSValue
-qjs_njs_getter(JSContext *ctx, JSValueConst this_val)
+JSValue
+qjs_call_exit_hook(JSContext *ctx)
 {
-    JSValue  obj;
+    JSValue    global, obj, ret;
+    qjs_njs_t  *njs;
 
-    obj = JS_NewObject(ctx);
+    global = JS_GetGlobalObject(ctx);
+
+    obj = JS_GetPropertyStr(ctx, global, "njs");
+    if (JS_IsException(obj) || JS_IsUndefined(obj)) {
+        goto done;
+    }
+
+    njs = JS_GetOpaque(obj, QJS_CORE_CLASS_ID_NJS);
+    if (njs != NULL && JS_IsFunction(ctx, njs->hooks[QJS_NJS_HOOK_EXIT])) {
+        ret = JS_Call(ctx, njs->hooks[QJS_NJS_HOOK_EXIT], JS_UNDEFINED,
+                      0, NULL);
+
+        JS_FreeValue(ctx, njs->hooks[QJS_NJS_HOOK_EXIT]);
+        njs->hooks[QJS_NJS_HOOK_EXIT] = JS_UNDEFINED;
+
+        if (JS_IsException(ret)) {
+            JS_FreeValue(ctx, obj);
+            JS_FreeValue(ctx, global);
+            return ret;
+        }
+
+        JS_FreeValue(ctx, ret);
+    }
+
+done:
+
+    JS_FreeValue(ctx, obj);
+    JS_FreeValue(ctx, global);
+
+    return JS_UNDEFINED;
+}
+
+
+static int
+qjs_add_intrinsic_njs(JSContext *cx, JSValueConst global)
+{
+    JSValue  obj, proto;
+
+    if (JS_NewClass(JS_GetRuntime(cx), QJS_CORE_CLASS_ID_NJS,
+                    &qjs_njs_class) < 0)
+    {
+        return -1;
+    }
+
+    proto = JS_NewObject(cx);
+    if (JS_IsException(proto)) {
+        return -1;
+    }
+
+    JS_SetPropertyFunctionList(cx, proto, qjs_njs_proto,
+                               njs_nitems(qjs_njs_proto));
+
+    JS_SetClassProto(cx, QJS_CORE_CLASS_ID_NJS, proto);
+
+    obj = JS_NewObjectClass(cx, QJS_CORE_CLASS_ID_NJS);
     if (JS_IsException(obj)) {
+        return -1;
+    }
+
+    if (JS_SetPropertyStr(cx, global, "njs", obj) < 0) {
+        JS_FreeValue(cx, obj);
+        return -1;
+    }
+
+    return 0;
+}
+
+
+static void
+qjs_njs_mark(JSRuntime *rt, JSValueConst val, JS_MarkFunc *mark_func)
+{
+    unsigned   i;
+    qjs_njs_t  *njs;
+
+    njs = JS_GetOpaque(val, QJS_CORE_CLASS_ID_NJS);
+    if (njs != NULL) {
+        for (i = 0; i < njs_nitems(njs->hooks); i++) {
+            JS_MarkValue(rt, njs->hooks[i], mark_func);
+        }
+    }
+}
+
+
+static void
+qjs_njs_finalizer(JSRuntime *rt, JSValue val)
+{
+    unsigned   i;
+    qjs_njs_t  *njs;
+
+    njs = JS_GetOpaque(val, QJS_CORE_CLASS_ID_NJS);
+    if (njs != NULL) {
+        for (i = 0; i < njs_nitems(njs->hooks); i++) {
+            JS_FreeValueRT(rt, njs->hooks[i]);
+        }
+
+        js_free_rt(rt, njs);
+    }
+}
+
+
+static JSValue
+qjs_njs_on(JSContext *ctx, JSValueConst this_val, int argc,
+    JSValueConst *argv)
+{
+    unsigned   i, n;
+    qjs_njs_t  *njs;
+    njs_str_t  name;
+
+    static const njs_str_t hooks[] = {
+        njs_str("exit"),
+    };
+
+    njs = JS_GetOpaque(this_val, QJS_CORE_CLASS_ID_NJS);
+    if (njs == NULL) {
+        njs = js_mallocz(ctx, sizeof(qjs_njs_t));
+        if (njs == NULL) {
+            return JS_ThrowOutOfMemory(ctx);
+        }
+
+        JS_SetOpaque(this_val, njs);
+    }
+
+    name.start = (u_char *) JS_ToCStringLen(ctx, &name.length, argv[0]);
+    if (name.start == NULL) {
         return JS_EXCEPTION;
     }
 
-    JS_SetPropertyFunctionList(ctx, obj, qjs_njs_proto,
-                               njs_nitems(qjs_njs_proto));
+    i = 0;
+    n = njs_nitems(hooks);
 
-    return obj;
+    while (i < n) {
+        if (njs_strstr_eq(&name, &hooks[i])) {
+            break;
+        }
+
+        i++;
+    }
+
+    if (i == n) {
+        JS_ThrowTypeError(ctx, "unknown hook \"%s\"", name.start);
+        JS_FreeCString(ctx, (const char *) name.start);
+        return JS_EXCEPTION;
+    }
+
+    JS_FreeCString(ctx, (const char *) name.start);
+
+    if (!JS_IsFunction(ctx, argv[1]) && !JS_IsNull(argv[1])) {
+        JS_ThrowTypeError(ctx, "callback is not a function or null");
+        return JS_EXCEPTION;
+    }
+
+    JS_FreeValue(ctx, njs->hooks[i]);
+    njs->hooks[i] = JS_DupValue(ctx, argv[1]);
+
+    return JS_UNDEFINED;
 }
 
 

src/qjs.h
@@ -27,6 +27,7 @@ enum {
     QJS_CORE_CLASS_ID_UINT8_ARRAY_CTOR,
     QJS_CORE_CLASS_ID_TEXT_DECODER,
     QJS_CORE_CLASS_ID_TEXT_ENCODER,
+    QJS_CORE_CLASS_ID_NJS,
     QJS_CORE_CLASS_ID_FS_STATS,
     QJS_CORE_CLASS_ID_FS_DIRENT,
     QJS_CORE_CLASS_ID_FS_FILEHANDLE,
@@ -49,6 +50,7 @@ typedef struct {
 
 
 JSContext *qjs_new_context(JSRuntime *rt, qjs_module_t **addons);
+JSValue qjs_call_exit_hook(JSContext *ctx);
 
 
 JSValue qjs_new_uint8_array(JSContext *ctx, int argc, JSValueConst *argv);

commit 4936209ba7819135e683e90f364b55cdfee23704

Fixed heap-use-after-free in js_set handler.
The issue was introduced in commit 04f6dfb (0.9.2) by moving VM
destruction from the pool cleanup handler to the http cleanup handler.

Moving VM destruction to the http cleanup handler broke js_set variable
usage during the log phase, because these variables are called after the
VM has been destroyed.

The fix is to move VM destruction back to the pool cleanup handler, but
use a temporary pool while njs.on('exit', ...) is executing.

This fixes #969 and #971 issues on Github.

nginx/ngx_http_js_module.c
@@ -1628,7 +1628,7 @@ static ngx_int_t
 ngx_http_js_init_vm(ngx_http_request_t *r, njs_int_t proto_id)
 {
     ngx_http_js_ctx_t       *ctx;
-    ngx_http_cleanup_t      *cln;
+    ngx_pool_cleanup_t      *cln;
     ngx_http_js_loc_conf_t  *jlcf;
 
     jlcf = ngx_http_get_module_loc_conf(r, ngx_http_js_module);
@@ -1663,7 +1663,7 @@ ngx_http_js_init_vm(ngx_http_request_t *r, njs_int_t proto_id)
                    "http js vm clone %s: %p from: %p", jlcf->engine->name,
                    ctx->engine, jlcf->engine);
 
-    cln = ngx_http_cleanup_add(r, 0);
+    cln = ngx_pool_cleanup_add(r->pool, 0);
     if (cln == NULL) {
         return NGX_ERROR;
     }
@@ -1701,7 +1701,16 @@ ngx_http_js_cleanup_ctx(void *data)
 
     jlcf = ngx_http_get_module_loc_conf(r, ngx_http_js_module);
 
+    /*
+     * r->pool set to NULL by ngx_http_free_request().
+     * Creating a temporary pool for potential use in njs.on('exit', ...)
+     * handler.
+     */
+    r->pool = ngx_create_pool(128, ctx->log);
+
     ngx_js_ctx_destroy((ngx_js_ctx_t *) ctx, (ngx_js_loc_conf_t *) jlcf);
+
+    ngx_destroy_pool(r->pool);
 }
 
 

nginx/t/js_exit.t
@@ -36,7 +36,13 @@ events {
 http {
     %%TEST_GLOBALS_HTTP%%
 
+    log_format test '[var:$bs header:$foo url:$url]';
+    access_log %%TESTDIR%%/access.log test;
+
     js_import test.js;
+    js_set $foo test.foo_header;
+    js_set $url test.url;
+    js_set $bs test.bytes_sent;
 
     server {
         listen       127.0.0.1:8080;
@@ -52,25 +58,44 @@ EOF
 
 $t->write_file('test.js', <<EOF);
     function test(r) {
-	    njs.on('exit', function() {
-		    ngx.log(ngx.WARN, `exit hook: bs: \${r.variables.bytes_sent}`);
-	    });
+        njs.on('exit', function() {
+            ngx.log(ngx.WARN, `exit hook: bs: \${r.variables.bytes_sent}`);
+        });
 
-	    r.return(200, `bs: \${r.variables.bytes_sent}`);
+        r.return(200, `bs: \${r.variables.bytes_sent}`);
     }
 
-    export default { test };
+    function bytes_sent(r) {
+        return r.variables.bytes_sent;
+    }
+
+    function foo_header(r) {
+        return Buffer.from(r.headersIn.foo).toString('hex');
+    }
+
+    function url(r) {
+        return r.uri;
+    }
+
+    export default { test, bytes_sent, foo_header, url };
 
 EOF
 
-$t->try_run('no njs')->plan(2);
+$t->try_run('no njs')->plan(3);
 
 ###############################################################################
 
-like(http_get('/test'), qr/bs: 0/, 'response');
+like(http(
+	'GET /test HTTP/1.0' . CRLF
+	. 'Foo: bar' . CRLF
+	. 'Host: localhost' . CRLF . CRLF
+), qr/bs: 0/, 'response');
 
 $t->stop();
 
-like($t->read_file('error.log'), qr/\[warn\].*exit hook: bs: \d+/, 'exit hook logged');
+like($t->read_file('error.log'), qr/\[warn\].*exit hook: bs: \d+/,
+	'exit hook logged');
+like($t->read_file('access.log'), qr/\[var:\d+ header:626172 url:\/test\]/,
+	'access log has bytes_sent');
 
 ###############################################################################

nginx/t/js_shared_dict_exit.t
@@ -1,0 +1,84 @@
+#!/usr/bin/perl
+
+# (C) Dmitry Volyntsev
+# (C) F5, Inc.
+
+# Tests for js_shared_dict_zone directive, njs.on('exit', ...).
+
+###############################################################################
+
+use warnings;
+use strict;
+
+use Test::More;
+use Socket qw/ CRLF /;
+
+BEGIN { use FindBin; chdir($FindBin::Bin); }
+
+use lib 'lib';
+use Test::Nginx;
+
+###############################################################################
+
+select STDERR; $| = 1;
+select STDOUT; $| = 1;
+
+my $t = Test::Nginx->new()->has(qw/http/)
+	->write_file_expand('nginx.conf', <<'EOF');
+
+%%TEST_GLOBALS%%
+
+daemon off;
+
+events {
+}
+
+http {
+    %%TEST_GLOBALS_HTTP%%
+
+    js_import test.js;
+
+    js_shared_dict_zone zone=bar:64k type=string;
+
+    server {
+        listen       127.0.0.1:8080;
+        server_name  localhost;
+
+        location /exit {
+            js_content test.exit;
+        }
+    }
+}
+
+EOF
+
+$t->write_file('test.js', <<'EOF');
+    function exit(r) {
+        var dict = ngx.shared.bar;
+        var key = r.args.key;
+
+        if (!dict.add(key, 'value')) {
+            r.return(200, `Key ${key} exists`);
+            return;
+        }
+
+        njs.on('exit', function() {
+            dict.delete(key);
+        });
+
+        r.return(200, 'SUCCESS');
+    }
+
+    export default { exit };
+EOF
+
+$t->try_run('no js_shared_dict_zone');
+
+$t->plan(2);
+
+###############################################################################
+
+like(http_get('/exit?key=foo'), qr/SUCCESS/, 'first');
+like(http_get('/exit?key=foo'), qr/SUCCESS/, 'second');
+
+###############################################################################