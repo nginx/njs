26aedbac7f9a3e6d32192a9aaf0e494c152f4a26
Dmitry Volyntsev <xeioex@nginx.com>
2025-10-14 17:42:07 -0700

    Fetch: introduced ngx_js_chain_to_buf() helper function.

--- nginx/ngx_js_http.h
+++ nginx/ngx_js_http.h
@@ -167,6 +167,8 @@ void ngx_js_http_trim(u_char **value, size_t *len,
     int trim_c0_control_or_space);
 ngx_int_t ngx_js_check_header_name(u_char *name, size_t len);
 
+ngx_buf_t *ngx_js_chain_to_buf(ngx_pool_t *pool, njs_chb_t *chain);
+
 void ngx_js_fetch_build_request(ngx_js_http_t *http, ngx_js_request_t *request,
     ngx_str_t *path, ngx_url_t *u);
 
